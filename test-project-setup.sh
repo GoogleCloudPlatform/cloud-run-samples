aac8e22153e18b2abaf125bcfdbcab37ae6bbb93
Dina Graves Portman <dinagraves@google.com>
2020-04-22 14:30:05 -0700

    Making TEAM_FOLDER optional and adding project flag to API enablement

--- test-project-setup.sh
+++ test-project-setup.sh
@@ -13,11 +13,15 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+set -e
 
 setup () {
   # TEAM FOLDER must be in set in local environment
   if [[ ! ${TEAM_FOLDER} ]]
-  then echo "TEAM_FOLDER missing"; exit
+  then echo "WARNING: You are creating this project without a Team Folder.\n\n\
+If you are creating this project in an organization, there may be \
+a policy on project folders. To move the project after completion \
+see https://cloud.google.com/resource-manager/docs/migrating-projects-billing";
   fi
 
   export PARENT_PROJECT=$(gcloud config get-value project)
@@ -29,25 +33,31 @@ setup () {
   export BILLING_ACCOUNT=$(gcloud beta billing projects describe ${PARENT_PROJECT} --format="value(billingAccountName)" || sed -e 's/.*\///g')
   # BILLING ACCOUNT required for API enablement
   if [[ ! ${BILLING_ACCOUNT} ]]
-  then echo "BILLING_ACCOUNT missing"; exit
+  then echo "BILLING_ACCOUNT missing. \
+            Please select an active project with a billing account, by running \
+            gcloud config set project {PROJECT_ID}"; exit
   fi
 
   export TESTING_PROJECT=$(printf "cloud-run-samples-test-%06d" $((RANDOM%999999)))
   export PROJECT_REGION=us-central1
 
   echo "Setting up project for Cloud Run Samples Testing..."; set -x
-  gcloud projects create ${TESTING_PROJECT} --folder=${TEAM_FOLDER}
+  if [[ ${TEAM_FOLDER} ]]
+  then gcloud projects create ${TESTING_PROJECT} --folder=${TEAM_FOLDER}
+  else gcloud projects create ${TESTING_PROJECT}
+  fi
+
   gcloud beta billing projects link ${TESTING_PROJECT} --billing-account=${BILLING_ACCOUNT}
-  gcloud config set project ${TESTING_PROJECT}
   set +x; echo
 
   echo "Enabling apis..."; set -x
-  gcloud services enable compute.googleapis.com
-  gcloud services enable run.googleapis.com
-  gcloud services enable cloudbuild.googleapis.com
-  gcloud services enable pubsub.googleapis.com
-  gcloud services enable containerregistry.googleapis.com
+  gcloud services enable run.googleapis.com --project $TESTING_PROJECT
+  gcloud services enable cloudbuild.googleapis.com --project $TESTING_PROJECT
+  gcloud services enable pubsub.googleapis.com --project $TESTING_PROJECT
+  gcloud services enable containerregistry.googleapis.com --project $TESTING_PROJECT
   set +x; echo
+
+  echo "Project setup complete"
 }
 
 setup
