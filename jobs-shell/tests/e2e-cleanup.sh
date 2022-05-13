3561dbbdaa35081eadc4932ad06fd59127a08c2d
Wietse Venema <356014+wietsevenema@users.noreply.github.com>
2022-05-13 15:33:21 +0200

    Clean up resources and tag image

--- jobs-shell/tests/e2e-cleanup.sh
+++ jobs-shell/tests/e2e-cleanup.sh
@@ -12,25 +12,9 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
-set -eux pipefail
-
-SHORT_SHA="${SHORT_SHA:-latest}"
+set -x
 
 gcloud config set run/region "${_REGION}"
-
 JOB_NAME="jobs-shell-${BUILD_ID}"
 
-gcloud builds submit -t "gcr.io/${PROJECT_ID}/jobs-shell:${SHORT_SHA}"
-
-gcloud alpha run jobs create "${JOB_NAME}" \
-  --set-env-vars FAIL_RATE=0,SLEEP_MS=10 \
-  --max-retries 2 \
-  --tasks 5 \
-  --image "gcr.io/${PROJECT_ID}/jobs-shell:${SHORT_SHA}"
-
-# Because of --wait, the command will fail if the 
-# execution fails, causing the entire script to fail
-gcloud alpha run jobs execute "${JOB_NAME}" \
-  --format=json \
-  --wait 
+gcloud alpha run jobs delete --quiet "${JOB_NAME}"
