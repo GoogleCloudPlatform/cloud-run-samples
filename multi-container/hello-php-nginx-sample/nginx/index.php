ccff6d27398b05e80c96d013e434c3c5b1933ba9
pattishin@google.com <pattishin@users.noreply.github.com>
2023-07-26 12:07:10 -0700

    refactor: add in region tags

--- multi-container/hello-php-nginx-sample/nginx/index.php
+++ multi-container/hello-php-nginx-sample/nginx/index.php
@@ -1,3 +1,5 @@
+# [START cloudrun_mc_hello_php_nginx_serving_index]
 <?php
   echo "This is nginx. Let's start processing requests!";
 ?>
+# [END cloudrun_mc_hello_php_nginx_serving_index]
--- multi-container/hello-php-nginx-sample/php-app/index.php
+++ multi-container/hello-php-nginx-sample/php-app/index.php
@@ -1,4 +1,6 @@
+# [START cloudrun_mc_hello_php_nginx_sidecar_index]
 <?php
   echo "This is main php app. Hello PHP World!";
   phpinfo();
 ?>
+# [END cloudrun_mc_hello_php_nginx_sidecar_index]
