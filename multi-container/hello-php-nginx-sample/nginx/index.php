a6dc54609bc5b1daf265b24f4a7431fcb8bd7226
Patti Shin <pattishin@users.noreply.github.com>
2023-07-28 11:56:29 -0700

    Update multi-container/hello-php-nginx-sample/nginx/index.php
    Co-authored-by: Julien Breux <julien.breux@gmail.com>

--- multi-container/hello-php-nginx-sample/nginx/index.php
+++ multi-container/hello-php-nginx-sample/nginx/index.php
@@ -1,5 +1,5 @@
-# [START cloudrun_mc_hello_php_nginx_serving_index]
 <?php
-  echo "This is nginx. Let's start processing requests!";
-?>
-# [END cloudrun_mc_hello_php_nginx_serving_index]
+// [START cloudrun_mc_hello_php_nginx_serving_index]
+echo 'This is nginx. Let's start processing requests!';
+
+// [END cloudrun_mc_hello_php_nginx_serving_index]
