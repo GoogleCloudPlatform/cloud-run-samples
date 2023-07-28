788be63545b4bf8fa1e3750d08df156d14f70e8c
Patti Shin <pattishin@users.noreply.github.com>
2023-07-28 11:56:38 -0700

    Update multi-container/hello-php-nginx-sample/php-app/index.php
    Co-authored-by: Julien Breux <julien.breux@gmail.com>

--- multi-container/hello-php-nginx-sample/php-app/index.php
+++ multi-container/hello-php-nginx-sample/php-app/index.php
@@ -1,6 +1,8 @@
-# [START cloudrun_mc_hello_php_nginx_sidecar_index]
 <?php
-  echo "This is main php app. Hello PHP World!";
-  phpinfo();
-?>
-# [END cloudrun_mc_hello_php_nginx_sidecar_index]
+// [START cloudrun_mc_hello_php_nginx_sidecar_index]
+
+echo 'This is main php app. Hello PHP World!';
+
+phpinfo();
+
+// [END cloudrun_mc_hello_php_nginx_sidecar_index]
