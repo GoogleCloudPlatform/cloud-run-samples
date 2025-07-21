24172b339e35f26c9ec2b78df92d11fc95261438
Katie McLaughlin <katie@glasnt.com>
2025-07-21 16:31:13 +1000

    headers, permissions

--- github-runner/worker-pool-container/start.sh
+++ github-runner/worker-pool-container/start.sh
@@ -1,10 +1,24 @@
 #!/bin/bash
+# Copyright 2025 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 
 set -e
 
 # Prepare internal variables.
 GITHUB_REPO_URL=https://github.com/${GITHUB_REPO}
-GH_TOKEN=$GITHUB_RUNNER_TOKEN
+GH_TOKEN=$GITHUB_TOKEN
 
 RUNNER_PREFIX="cloud-run-worker"
 RUNNER_SUFFIX=$(cat /dev/urandom | tr -dc 'a-z0-9' | fold -w 5 | head -n 1)
