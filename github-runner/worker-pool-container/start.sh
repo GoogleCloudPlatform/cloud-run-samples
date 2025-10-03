56dbe4ad1a459352905ba80ce1daf97fcb87b2a5
Katie McLaughlin <katie@glasnt.com>
2025-10-03 13:40:17 +1000

    new tutorial: hosted github runners with cloud run worker pools (#288)
    * WIP: based on python-docs-samples #13481
    
    * Rework
    
    * apply dockerlint suggestion
    
    * update version
    
    * remove unused var
    
    * Update scaling logic to scale to zero when no pending jobs
    
    * Use persistent runners (prevents reboot loops)
    
    * correct regiontag prefix
    
    * lint
    
    * ignore hadolint warning
    
    This Dockerfile inherits from the actions-runner, a complex conatiner.
    It sets it's own WORKDIR, which I am inheriting for this copy, and I
    don't want to potentially break this configuration by changing this
    value.
    
    * update actions-runner to latest

---
+++ github-runner/worker-pool-container/start.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+# Copyright 2025 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+set -e
+
+# Prepare internal variables.
+GITHUB_REPO_URL=https://github.com/${GITHUB_REPO}
+GH_TOKEN=$GITHUB_TOKEN
+
+RUNNER_PREFIX="cloud-run-worker"
+RUNNER_SUFFIX=$(cat /dev/urandom | tr -dc 'a-z0-9' | fold -w 5 | head -n 1)
+RUNNER_NAME="${RUNNER_PREFIX}-${RUNNER_SUFFIX}"
+
+# [START cloudrun_github_worker_pool_start]
+# Configure the current runner instance with URL, token and name.
+mkdir /home/docker/actions-runner && cd /home/docker/actions-runner
+echo "GitHub Repo: ${GITHUB_REPO_URL} for ${RUNNER_PREFIX}-${RUNNER_SUFFIX}"
+./config.sh --unattended --url ${GITHUB_REPO_URL} --pat ${GH_TOKEN} --name ${RUNNER_NAME}
+
+# Function to cleanup and remove runner from Github.
+cleanup() {
+   echo "Removing runner..."
+   ./config.sh remove --unattended --pat ${GH_TOKEN}
+}
+
+# Trap signals.
+trap 'cleanup; exit 130' INT
+trap 'cleanup; exit 143' TERM
+
+# Run the runner.
+./run.sh & wait $!
+
+# [END cloudrun_github_worker_pool_start]
