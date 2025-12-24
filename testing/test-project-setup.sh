852d5a634e63c929ec2318de45e203ac15394cd4
bhandarivijay <bhandarivijay@google.com>
2025-12-24 21:32:23 +0000

    Migrate gsutil usage to gcloud storage

--- testing/test-project-setup.sh
+++ testing/test-project-setup.sh
@@ -86,7 +86,7 @@ https://cloud.google.com/resource-manager/docs/migrating-projects-billing";
     --role roles/secretmanager.secretAccessor
 
   echo "Creating Cloud Storage bucket for sample integration"
-  gsutil mb gs://${TESTING_PROJECT}
+  gcloud storage buckets create gs://${TESTING_PROJECT}
   // Delete objects in this bucket after 3 days.
   gcloud storage buckets update gs://${TESTING_PROJECT} --lifecycle-file=sample-storage.lifecycle.json
 
